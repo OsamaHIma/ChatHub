3fc9c06f75b757cfd623a1e2486709e832e097ae
Osama Ibrahim <108794970+OsamaHIma@users.noreply.github.com>
2023-10-17 19:34:21 +0300

    push

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useRef, useState } from "react";
-import { Send, SmileIcon, PaperclipIcon } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import { Send, SmileIcon, PaperclipIcon, Reply, X } from "lucide-react";
 import {
   IconButton,
   Textarea,
@@ -12,7 +12,7 @@ import {
 import Picker from "emoji-picker-react";
 import { useUser } from "@/context/UserContext";
 
-const ChatInput = ({ handleSendMsg,socket }) => {
+const ChatInput = ({ handleSendMsg, socket, isRelyingToMessage, currentChat, setIsRelyingToMessage }) => {
   const [msg, setMsg] = useState("");
   const { user } = useUser();
   const handelEmojiPickerClick = (emoji, event) => {
@@ -21,86 +21,119 @@ const ChatInput = ({ handleSendMsg,socket }) => {
     setMsg((prevMsg) => prevMsg + emojiString);
   };
 
-  const fileInputRef = useRef();
+  const fileInputRef = useRef(null);
+  const textAreaRef = useRef(null);
   const handleSubmit = (event) => {
     event.preventDefault();
     if (msg.length > 0) {
       socket.current.emit("stop-typing", user._id);
       handleSendMsg(msg);
+      setIsRelyingToMessage(null)
       setMsg("");
     }
   };
-
+  useEffect(() => {
+   
+    if (isRelyingToMessage) {
+      textAreaRef.current.getElementsByTagName("textarea")[0].focus();
+    }
+  }, [isRelyingToMessage]);
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="mt-5 flex min-w-[15.5rem] items-center gap-5"
-      noValidate
-    >
-      <div className="flex w-full flex-row items-center gap-2 rounded-full border border-gray-900/10 bg-gray-900/5 p-2 dark:border-gray-100/10 dark:bg-gray-800">
-        <div className="flex">
-          <input
-            type="file"
-            // accept="image/*"
-            ref={fileInputRef}
-            style={{ display: "none" }}
-            // onChange={(e) => setSelectedImage(e.target.files[0])}
-          />
+    <>
+      {
+        isRelyingToMessage && (
+          <div className="flex relative items-center justify-between max-w-full mt-1">
+            <div className="flex items-center gap-7 max-w-xs">
+              <div className="flex items-center gap-3 flex-nowrap">
+                <Reply className="text-indigo-600 min-w-[2rem]" />
+                <p>
+                  {currentChat && currentChat.username}
+                </p>
+                <span className="text-indigo-500">|</span>
+              </div>
+              <p className="px-3 py-1 bg-gray-100 truncate dark:bg-gray-700 rounded-md dark:text-gray-100">
+                {isRelyingToMessage.message}
+              </p>
+            </div>
+            <IconButton className="rounded-full self-end !outline-none" variant="text" onClick={() => setIsRelyingToMessage(null)}>
+              <X className="dark:text-slate-50" />
+            </IconButton>
+          </div>
+        )
+      }
+      <form
+        onSubmit={handleSubmit}
+        className={`${isRelyingToMessage ? "mt-1" : " mt-5"} relative flex min-w-[15.5rem] items-center gap-5`}
+        noValidate
+      >
 
-          <IconButton
-            variant="text"
-            className="relative rounded-full"
-            onClick={() => fileInputRef.current.click()}
-          >
-            {/* {selectedImage && (
-                          <div className="absolute inset-0 -top-1 h-3 w-3 rounded-full bg-green-600"></div>
-                        )} */}
-            <PaperclipIcon className="dark:text-stone-300" />
-          </IconButton>
-        </div>
-        <Menu
-          animate={{
-            mount: { y: 0 },
-            unmount: { y: 25 },
-          }}
-        >
-          <MenuHandler>
+        <div className="flex w-full flex-row items-center gap-2 rounded-full border border-gray-900/10 bg-gray-900/5 p-2 dark:border-gray-100/10 dark:bg-gray-800">
+          <div className="flex">
+            <input
+              type="file"
+              // accept="image/*"
+              ref={fileInputRef}
+              style={{ display: "none" }}
+            // onChange={(e) => setSelectedImage(e.target.files[0])}
+            />
+
             <IconButton
               variant="text"
               className="relative rounded-full"
-              type="button"
-              //   onClick={handelEmojiPickerToggle}
+              onClick={() => fileInputRef.current.click()}
             >
-              <SmileIcon className="inline-block dark:text-slate-50" />
+              {/* {selectedImage && (
+                          <div className="absolute inset-0 -top-1 h-3 w-3 rounded-full bg-green-600"></div>
+                        )} */}
+              <PaperclipIcon className="dark:text-stone-300" />
             </IconButton>
-          </MenuHandler>
-          <MenuList>
-            <Picker onEmojiClick={handelEmojiPickerClick} />
-          </MenuList>
-        </Menu>
-        <Textarea
-          rows={1}
-          resize={true}
-          value={msg}
-          onChange={(e) => {
-            socket.current.emit("typing", user._id);
-            setMsg(e.target.value);
-          }}
-          placeholder="Your Message"
-          className="bg min-h-full !border-0 focus:border-transparent dark:text-gray-300 dark:placeholder:text-gray-400"
-          containerProps={{
-            className: "grid h-full",
-          }}
-          labelProps={{
-            className: "before:content-none after:content-none",
-          }}
-        />
+          </div>
+          <Menu
+            animate={{
+              mount: { y: 0 },
+              unmount: { y: 25 },
+            }}
+          >
+            <MenuHandler>
+              <IconButton
+                variant="text"
+                className="relative rounded-full"
+                type="button"
+              //   onClick={handelEmojiPickerToggle}
+              >
+                <SmileIcon className="inline-block dark:text-slate-50" />
+              </IconButton>
+            </MenuHandler>
+            <MenuList>
+              <Picker onEmojiClick={handelEmojiPickerClick} />
+            </MenuList>
+          </Menu>
+          <Textarea
+            ref={textAreaRef}
+            rows={1}
+            resize={true}
+            value={msg}
+            onChange={(e) => {
+              socket.current.emit("typing", user._id);
+              setMsg(e.target.value);
+            }}
 
-        <IconButton variant="text" className="rounded-full" type="submit">
-          <Send size={23} className=" inline-block dark:text-slate-50" />
-        </IconButton>
-      </div>
-    </form>
+            placeholder="Your Message"
+            className="bg min-h-full !border-0 focus:border-transparent dark:text-gray-300 dark:placeholder:text-gray-400"
+            containerProps={{
+              className: "grid h-full",
+            }}
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+          />
+
+          <IconButton variant="text" className="rounded-full" type="submit">
+            <Send size={23} className=" inline-block dark:text-slate-50" />
+          </IconButton>
+        </div>
+      </form>
+    </>
   );
 };
 
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -6,18 +6,19 @@ import ChatInput from "./ChatInput";
 import { useUser } from "@/context/UserContext";
 import axios from "axios";
 import { Button, Spinner } from "@material-tailwind/react";
-import { CheckCheck } from "lucide-react";
+import { CheckCheck, Reply } from "lucide-react";
 import ScrollableFeed from "react-scrollable-feed";
 import { Howl } from "howler";
 import { useInfiniteQuery } from "@tanstack/react-query";
+import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
 
 const Messages = ({ currentChat, socket }) => {
   const { user } = useUser();
   const [messages, setMessages] = useState([]);
-  // const [loading, setIsLoading] = useState(false);
+  const [loading, setIsLoading] = useState(false);
   const [arrivalMessages, setArrivalMessages] = useState(null);
+  const [isRelyingToMessage, setIsRelyingToMessage] = useState(null);
   const notificationSound = new Howl({ src: ["/chat.mp3"], volume: 0.5 });
-
 
   const getAllMsgBetweenTowUsers = async (page) => {
     try {
@@ -31,13 +32,33 @@ const Messages = ({ currentChat, socket }) => {
         },
       );
       // setIsLoading(false);
+      console.log(data)
+      return data;
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const getMsgBetweenTowUsersWhenLoaded = async (page) => {
+    try {
+      setIsLoading(true);
+      const { data } = await axios.post(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/messages/get_all_msgs_between_tow_users`,
+        {
+          from: user._id,
+          to: currentChat._id,
+          page: page,
+        },
+      );
+      setIsLoading(false);
+      console.log(data)
       return data;
     } catch (error) {
       console.error(error);
     }
   };
 
-  const { data, fetchNextPage, isFetchingNextPage, refetch, hasNextPage } =
+  const { data, fetchNextPage, isFetchingNextPage, refetch } =
     useInfiniteQuery(
       ["query"],
       async ({ pageParam = 1 }) => {
@@ -50,7 +71,7 @@ const Messages = ({ currentChat, socket }) => {
         },
         initialData: {
           pages: async () => {
-            const response = await getAllMsgBetweenTowUsers(1);
+            const response = await getMsgBetweenTowUsersWhenLoaded(1);
             return response;
           },
           pageParams: [1],
@@ -68,6 +89,7 @@ const Messages = ({ currentChat, socket }) => {
         from: user._id,
         date: time,
         message,
+        replyTo: isRelyingToMessage ? isRelyingToMessage._id : null
       },
     );
 
@@ -77,6 +99,7 @@ const Messages = ({ currentChat, socket }) => {
       from: user._id,
       date: time,
       message,
+      replyTo: isRelyingToMessage ? isRelyingToMessage._id : null,
       ...data,
     });
 
@@ -99,7 +122,6 @@ const Messages = ({ currentChat, socket }) => {
     arrivalMessages && setMessages((prev) => [arrivalMessages, ...prev]);
   }, [arrivalMessages]);
 
-
   useEffect(() => {
     if (data.pages.length > 0) {
       setMessages(data?.pages.flatMap((page) => page));
@@ -108,46 +130,103 @@ const Messages = ({ currentChat, socket }) => {
 
   useEffect(() => {
     refetch();
-  }, [currentChat,messages]);
+  }, [currentChat, messages]);
 
-  const ComingMessage = ({ message }) => {
+  const ComingMessage = ({ message, index }) => {
     const time = moment(message.date).format("hh:mm a");
     return (
-      <div className="mb-8 flex">
-        <div className="max-w-1/2 relative ml-4 rounded-lg bg-gray-700 px-6 py-4">
-          <p className="whitespace-normal break-all text-white">
-            <Translate>{message.message}</Translate>
-          </p>
-          <p className="absolute bottom-0 right-0 -mb-5 text-xs text-gray-400">
-            {time}
-          </p>
-        </div>
-      </div>
+      <>
+        <ContextMenuTrigger id={index}>
+          <div
+            className="relative mb-8 flex">
+            <div className="max-w-1/2 ml-4 rounded-lg bg-gray-700 px-6 py-4">
+              {
+                message.replyToMessage && (
+                  <div className="max-w-xs mb-1">
+                    <p className="p-3 truncate bg-green-500/70 rounded-md text-gray-100">
+                      {message.replyToMessage.content}
+                    </p>
+                  </div>
+                )
+              }
+              <p className="whitespace-normal break-all text-white">
+                <Translate>{message.message}</Translate>
+              </p>
+              <p className="absolute bottom-0 left-5 -mb-5 text-xs text-gray-400">
+                {time}
+              </p>
+            </div>
+          </div>
+        </ContextMenuTrigger>
+
+        <ContextMenu
+          id={index}
+        // className="border-1 rounded-lg p-3 dark:bg-gray-700"
+        >
+          <MenuItem
+            className="cursor-pointer rounded-md bg-gray-100 px-3 py-2 text-gray-800 shadow dark:bg-gray-800 dark:text-gray-50"
+            onClick={() => setIsRelyingToMessage(message)}
+          >
+            Reply
+          </MenuItem>
+        </ContextMenu>
+      </>
     );
   };
 
-  const SendMessage = ({ message }) => {
+  const SendMessage = ({ message, index }) => {
     const time = moment(message.date).format("hh:mm a");
     return (
-      <div className="mb-8 flex flex-row-reverse">
-        <div className="max-w-1/2 relative mr-4 rounded-lg bg-green-400 bg-opacity-60 px-6 py-4">
-          <p className="whitespace-normal break-all">{message.message}</p>
-          <div className="absolute bottom-0 right-0 -mb-5 flex items-center gap-1">
-            <p className="text-xs text-gray-400">{time}</p>
-            <CheckCheck
-              className={`${message.seen ? "text-blue-500" : "text-gray-400"} `}
-              size={17}
-            />
+      <>
+        <ContextMenuTrigger id={index}>
+          <div className="mb-8 relative max-w-full flex flex-row-reverse">
+            <div className="max-w-1/2 mr-4 rounded-lg bg-green-400 bg-opacity-60 px-6 py-4">
+              {
+                message.replyToMessage && (
+                  <div className="max-w-xs mb-1">
+                    <p className="p-3 truncate bg-green-500/70 rounded-md text-gray-100">
+                      {message.replyToMessage.content}
+                    </p>
+                  </div>
+                )
+              }
+              <p className="whitespace-normal break-all">{message.message}</p>
+            </div>
+            <div className="absolute bottom-0 right-0 -mb-5 flex items-center gap-1 flex-nowrap">
+              <p className="text-xs text-gray-400">{time}</p>
+              <CheckCheck
+                className={`${message.seen ? "text-blue-500" : "text-gray-400"} `}
+                size={17}
+              />
+            </div>
           </div>
-        </div>
-      </div>
+        </ContextMenuTrigger>
+        <ContextMenu
+          id={index}
+        // className="border-1 rounded-lg p-3 dark:bg-gray-700"
+        >
+          <MenuItem
+            className="cursor-pointer rounded-md bg-gray-100 px-3 py-2 text-gray-800 shadow dark:bg-gray-800 dark:text-gray-50"
+            onClick={() => setIsRelyingToMessage(message)}
+          >
+            Reply
+          </MenuItem>
+        </ContextMenu>
+
+      </>
     );
   };
   return (
     <section className="mb-7 md:mb-0">
       <div className="flex flex-col justify-between gap-7 overflow-y-auto">
         <div className="relative mt-4 flex flex-col justify-between">
-          <Button variant="text" color="indigo" onClick={() => fetchNextPage()} className="!absolute max-w-fit -top-7 dark:text-slate-100 left-[30%] md:left-[35%] z-50" disabled={isFetchingNextPage}>
+          <Button
+            variant="text"
+            color="indigo"
+            onClick={() => fetchNextPage()}
+            className="!absolute -top-7 left-[30%] z-50 max-w-fit dark:text-slate-100 md:left-[35%]"
+            disabled={isFetchingNextPage}
+          >
             {isFetchingNextPage ? (
               <Spinner color="green" className="mx-auto" />
             ) : (
@@ -155,9 +234,9 @@ const Messages = ({ currentChat, socket }) => {
             )}
           </Button>
           <div className="hide-scroll-bar relative h-[78vh] overflow-y-auto rounded-lg bg-slate-200/50 p-6 dark:bg-slate-900/50 md:h-[70vh]">
-            {/* {loading && (
+            {loading && (
               <Spinner scale={7} className="absolute left-[50%] top-[50%]" />
-            )} */}
+            )}
             <ScrollableFeed>
               {messages.length > 0 &&
                 messages
@@ -165,16 +244,24 @@ const Messages = ({ currentChat, socket }) => {
                   .reverse()
                   .map((message, index) =>
                     message.fromSelf ? (
-                      <SendMessage key={index} message={message} />
+                      <SendMessage
+                        key={index}
+                        index={index}
+                        message={message}
+                      />
                     ) : (
-                      <ComingMessage key={index} message={message} />
+                      <ComingMessage
+                        key={index}
+                        index={index}
+                        message={message}
+                      />
                     ),
                   )}
             </ScrollableFeed>
           </div>
         </div>
       </div>
-      <ChatInput socket={socket} handleSendMsg={sendMessage} />
+      <ChatInput socket={socket} handleSendMsg={sendMessage} currentChat={currentChat} isRelyingToMessage={isRelyingToMessage} setIsRelyingToMessage={setIsRelyingToMessage} />
     </section>
   );
 };
