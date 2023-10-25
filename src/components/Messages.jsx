e2e87f599488c0c0687042cf8ee8698f61f1041e
Osama Ibrahim <108794970+OsamaHIma@users.noreply.github.com>
2023-10-25 11:36:29 +0300

    push

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -168,205 +168,203 @@ const Messages = ({ currentChat, socket }) => {
     if (!id) return;
     const targetMessage = typeof document !== "undefined" &&
       document.getElementById(id)
-  }
+    if (targetMessage) {
+      targetMessage.scrollIntoView({ behavior: "smooth" });
+      targetMessage.classList.add("highlighted");
 
-  if (targetMessage) {
-    targetMessage.scrollIntoView({ behavior: "smooth" });
-    targetMessage.classList.add("highlighted");
+      setTimeout(() => {
+        targetMessage.classList.remove("highlighted");
+      }, 3000);
+    }
+  };
 
-    setTimeout(() => {
-      targetMessage.classList.remove("highlighted");
-    }, 3000);
-  }
-};
+  const ComingMessage = ({ message, index }) => {
+    const time = moment(message.date).format("hh:mm a");
+    return (
+      <>
+        <ContextMenuTrigger className="!z-[100]" id={index} holdToDisplay={900} >
+          <div
+            className="relative mb-8 flex py-2" id={message._id}>
+            {
+              message.fileName && (
+                <IconButton className="rounded-full bg-gray-100 dark:bg-gray-900 !absolute -top-2 left-2 !outline-none" onClick={() => setIsRelyingToMessage(null)}>
+                  <Link
+                    href={message.fileURL}
+                    download={message.fileName}
+                    target="_blank"
+                  >
+                    <Download className="text-gray-900 dark:text-slate-50" />
+                  </Link>
+                </IconButton>
+              )}
+            <div className="max-w-fit ml-4 rounded-lg text-gray-50 bg-gray-700 px-4 py-3">
+              {
+                message.replyToMessage && (
+                  <div className="max-w-[13rem] md:max-w-xs mb-1 cursor-pointer" onClick={() => handelReplyMessageClick(message.replyTo)}>
+                    <p className={`p-3 truncate ${message.replyToMessage.sender === currentChat._id ? "bg-gray-200 text-gray-800" : "bg-green-500/70 text-gray-100"} rounded-md `}>
+                      {message.replyToMessage.content}
+                    </p>
+                  </div>
+                )
+              }
+              {
+                message.fileURL && (
+                  <embed onClick={(e) => handleOpenFile(e, message.fileURL)} src={message.fileURL} className="max-w-[13rem] md:max-w-xs cursor-pointer rounded-md mb-2" />
+                )
+              }
+              <div className="whitespace-normal break-all" dangerouslySetInnerHTML={{ __html: message.message }} />
+              <p className="absolute bottom-0 left-5 -mb-5 text-xs text-gray-400">
+                {time}
+              </p>
+            </div>
+          </div>
+        </ContextMenuTrigger>
 
-const ComingMessage = ({ message, index }) => {
-  const time = moment(message.date).format("hh:mm a");
+        <ContextMenu
+          id={index && index}
+          className="!z-[100] bg-gray-200 dark:bg-gray-700 p-3 rounded-lg flex flex-col gap-3"
+        >
+          <MenuItem
+            className="cursor-pointer flex items-center gap-3 z-50 rounded-md bg-gray-100 px-3 py-2 text-gray-800 shadow dark:bg-gray-800 dark:text-gray-50"
+            onClick={() => setIsRelyingToMessage(message)}
+          >
+            <Reply /> <span>Reply</span>
+          </MenuItem>
+          <MenuItem
+            onClick={async () => await navigator.clipboard.writeText(message.message.textContent || message.message)}
+            className="cursor-pointer flex items-center gap-3  z-50 rounded-md bg-gray-100 px-3 py-2 text-gray-800 shadow dark:bg-gray-800 dark:text-gray-50"
+          >
+            <ClipboardCopy />
+            <span>Copy</span>
+          </MenuItem>
+        </ContextMenu>
+      </>
+    );
+  };
+
+  const SendMessage = ({ message, index }) => {
+    const time = moment(message.date).format("hh:mm a");
+    return (
+      <>
+        <ContextMenuTrigger className="!z-[100]" id={index} holdToDisplay={900}  >
+          <div className="mb-8 relative max-w-full flex flex-row-reverse py-2" id={message._id}
+          >
+            {
+              message.fileName && (
+                <IconButton className="rounded-full bg-gray-100 dark:bg-gray-900 !absolute -top-2 right-2 !outline-none" onClick={() => setIsRelyingToMessage(null)}>
+                  <Link
+                    href={message.fileURL}
+                    download={message.fileName}
+                    target="_blank"
+                  >
+                    <Download className="text-gray-900 dark:text-slate-50" />
+                  </Link>
+                </IconButton>
+              )}
+            <div className="max-w-1/2 mr-4 rounded-lg bg-green-400 bg-opacity-60 px-4 py-3">
+              {
+                message.replyToMessage && (
+                  <div className="max-w-[13rem] md:max-w-xs mb-1 cursor-pointer" onClick={() => handelReplyMessageClick(message.replyTo)}>
+                    <p className={`p-3 truncate ${message.replyToMessage.sender === user._id ? "bg-green-500/70" : "bg-gray-700"}  rounded-md text-gray-100`}>
+                      {message.replyToMessage.content}
+                    </p>
+                  </div>
+                )
+              }
+              {
+                message.fileURL && (
+                  <embed onClick={(e) => handleOpenFile(e, message.fileURL)} src={message.fileURL} className="max-w-[13rem] md:max-w-xs cursor-pointer rounded-md mb-2" />
+                )
+              }
+              <div className="whitespace-normal break-all" dangerouslySetInnerHTML={{ __html: message.message }} />
+            </div>
+            <div className="absolute bottom-0 right-0 -mb-5 flex items-center gap-1 flex-nowrap">
+              <p className="text-xs text-gray-400">{time}</p>
+              <CheckCheck
+                className={`${message.seen ? "text-blue-500" : "text-gray-400"} `}
+                size={17}
+              />
+            </div>
+          </div>
+        </ContextMenuTrigger>
+        <ContextMenu
+          id={index && index}
+          className="!z-[100] bg-gray-200 dark:bg-gray-700 p-3 rounded-lg flex flex-col gap-3"
+        >
+          <MenuItem
+            className="cursor-pointer flex items-center gap-3 z-50 rounded-md bg-gray-100 px-3 py-2 text-gray-800 shadow dark:bg-gray-800 dark:text-gray-50"
+            onClick={() => setIsRelyingToMessage(message)}
+          >
+            <Reply />
+            <span>Reply</span>
+          </MenuItem>
+          <MenuItem
+            onClick={async () => await navigator.clipboard.writeText(message.message)}
+            className="cursor-pointer flex items-center gap-3  z-50 rounded-md bg-gray-100 px-3 py-2 text-gray-800 shadow dark:bg-gray-800 dark:text-gray-50"
+          >
+            <ClipboardCopy />
+            <span>Copy</span>
+          </MenuItem>
+          <MenuItem
+            onClick={() => deleteMessage(message._id)}
+            className="cursor-pointer flex items-center gap-3  z-50 rounded-md bg-gray-100 px-3 py-2 text-gray-800 shadow dark:bg-gray-800 dark:text-gray-50"
+          >
+            <Trash2Icon />
+            <span>Delete</span>
+          </MenuItem>
+        </ContextMenu>
+
+      </>
+    );
+  };
+
   return (
-    <>
-      <ContextMenuTrigger className="!z-[100]" id={index} holdToDisplay={900} >
-        <div
-          className="relative mb-8 flex py-2" id={message._id}>
-          {
-            message.fileName && (
-              <IconButton className="rounded-full bg-gray-100 dark:bg-gray-900 !absolute -top-2 left-2 !outline-none" onClick={() => setIsRelyingToMessage(null)}>
-                <Link
-                  href={message.fileURL}
-                  download={message.fileName}
-                  target="_blank"
-                >
-                  <Download className="text-gray-900 dark:text-slate-50" />
-                </Link>
-              </IconButton>
+    <section className="mb-7 md:mb-0">
+      <FullFile openFile={openFullFile} url={fileUrl} handleOpenFile={handleOpenFile} />
+      <div className="flex flex-col justify-between gap-7 overflow-y-auto">
+        <div className="relative mt-4 flex flex-col justify-between">
+          <Button
+            variant="text"
+            color="indigo"
+            onClick={() => fetchNextPage()}
+            className="!absolute -top-7 left-[20%] z-50 max-w-fit dark:text-slate-100 md:left-[35%]"
+            disabled={isFetchingNextPage}
+          >
+            {isFetchingNextPage ? (
+              <Spinner color="green" className="mx-auto" />
+            ) : (
+              <Translate>Load more older messages</Translate>
             )}
-          <div className="max-w-fit ml-4 rounded-lg text-gray-50 bg-gray-700 px-4 py-3">
-            {
-              message.replyToMessage && (
-                <div className="max-w-[13rem] md:max-w-xs mb-1 cursor-pointer" onClick={() => handelReplyMessageClick(message.replyTo)}>
-                  <p className={`p-3 truncate ${message.replyToMessage.sender === currentChat._id ? "bg-gray-200 text-gray-800" : "bg-green-500/70 text-gray-100"} rounded-md `}>
-                    {message.replyToMessage.content}
-                  </p>
-                </div>
-              )
-            }
-            {
-              message.fileURL && (
-                <embed onClick={(e) => handleOpenFile(e, message.fileURL)} src={message.fileURL} className="max-w-[13rem] md:max-w-xs cursor-pointer rounded-md mb-2" />
-              )
-            }
-            <div className="whitespace-normal break-all" dangerouslySetInnerHTML={{ __html: message.message }} />
-            <p className="absolute bottom-0 left-5 -mb-5 text-xs text-gray-400">
-              {time}
-            </p>
+          </Button>
+          <div className="hide-scroll-bar relative h-[78vh] overflow-y-auto rounded-lg bg-slate-200/50 py-6 px-1 dark:bg-slate-900/50 md:h-[70vh]">
+            <ScrollableFeed>
+              {messages.length > 0 &&
+                messages
+                  .slice()
+                  .reverse()
+                  .map((message, index) =>
+                    message.fromSelf ? (
+                      <SendMessage
+                        key={index}
+                        index={index}
+                        message={message}
+                      />
+                    ) : (
+                      <ComingMessage
+                        key={index}
+                        index={index}
+                        message={message}
+                      />
+                    ),
+                  )}
+
+            </ScrollableFeed>
           </div>
-        </div>
-      </ContextMenuTrigger>
-
-      <ContextMenu
-        id={index && index}
-        className="!z-[100] bg-gray-200 dark:bg-gray-700 p-3 rounded-lg flex flex-col gap-3"
-      >
-        <MenuItem
-          className="cursor-pointer flex items-center gap-3 z-50 rounded-md bg-gray-100 px-3 py-2 text-gray-800 shadow dark:bg-gray-800 dark:text-gray-50"
-          onClick={() => setIsRelyingToMessage(message)}
-        >
-          <Reply /> <span>Reply</span>
-        </MenuItem>
-        <MenuItem
-          onClick={async () => await navigator.clipboard.writeText(message.message.textContent || message.message)}
-          className="cursor-pointer flex items-center gap-3  z-50 rounded-md bg-gray-100 px-3 py-2 text-gray-800 shadow dark:bg-gray-800 dark:text-gray-50"
-        >
-          <ClipboardCopy />
-          <span>Copy</span>
-        </MenuItem>
-      </ContextMenu>
-    </>
-  );
-};
-
-const SendMessage = ({ message, index }) => {
-  const time = moment(message.date).format("hh:mm a");
-  return (
-    <>
-      <ContextMenuTrigger className="!z-[100]" id={index} holdToDisplay={900}  >
-        <div className="mb-8 relative max-w-full flex flex-row-reverse py-2" id={message._id}
-        >
-          {
-            message.fileName && (
-              <IconButton className="rounded-full bg-gray-100 dark:bg-gray-900 !absolute -top-2 right-2 !outline-none" onClick={() => setIsRelyingToMessage(null)}>
-                <Link
-                  href={message.fileURL}
-                  download={message.fileName}
-                  target="_blank"
-                >
-                  <Download className="text-gray-900 dark:text-slate-50" />
-                </Link>
-              </IconButton>
-            )}
-          <div className="max-w-1/2 mr-4 rounded-lg bg-green-400 bg-opacity-60 px-4 py-3">
-            {
-              message.replyToMessage && (
-                <div className="max-w-[13rem] md:max-w-xs mb-1 cursor-pointer" onClick={() => handelReplyMessageClick(message.replyTo)}>
-                  <p className={`p-3 truncate ${message.replyToMessage.sender === user._id ? "bg-green-500/70" : "bg-gray-700"}  rounded-md text-gray-100`}>
-                    {message.replyToMessage.content}
-                  </p>
-                </div>
-              )
-            }
-            {
-              message.fileURL && (
-                <embed onClick={(e) => handleOpenFile(e, message.fileURL)} src={message.fileURL} className="max-w-[13rem] md:max-w-xs cursor-pointer rounded-md mb-2" />
-              )
-            }
-            <div className="whitespace-normal break-all" dangerouslySetInnerHTML={{ __html: message.message }} />
-          </div>
-          <div className="absolute bottom-0 right-0 -mb-5 flex items-center gap-1 flex-nowrap">
-            <p className="text-xs text-gray-400">{time}</p>
-            <CheckCheck
-              className={`${message.seen ? "text-blue-500" : "text-gray-400"} `}
-              size={17}
-            />
-          </div>
-        </div>
-      </ContextMenuTrigger>
-      <ContextMenu
-        id={index && index}
-        className="!z-[100] bg-gray-200 dark:bg-gray-700 p-3 rounded-lg flex flex-col gap-3"
-      >
-        <MenuItem
-          className="cursor-pointer flex items-center gap-3 z-50 rounded-md bg-gray-100 px-3 py-2 text-gray-800 shadow dark:bg-gray-800 dark:text-gray-50"
-          onClick={() => setIsRelyingToMessage(message)}
-        >
-          <Reply />
-          <span>Reply</span>
-        </MenuItem>
-        <MenuItem
-          onClick={async () => await navigator.clipboard.writeText(message.message)}
-          className="cursor-pointer flex items-center gap-3  z-50 rounded-md bg-gray-100 px-3 py-2 text-gray-800 shadow dark:bg-gray-800 dark:text-gray-50"
-        >
-          <ClipboardCopy />
-          <span>Copy</span>
-        </MenuItem>
-        <MenuItem
-          onClick={() => deleteMessage(message._id)}
-          className="cursor-pointer flex items-center gap-3  z-50 rounded-md bg-gray-100 px-3 py-2 text-gray-800 shadow dark:bg-gray-800 dark:text-gray-50"
-        >
-          <Trash2Icon />
-          <span>Delete</span>
-        </MenuItem>
-      </ContextMenu>
-
-    </>
-  );
-};
-
-return (
-  <section className="mb-7 md:mb-0">
-    <FullFile openFile={openFullFile} url={fileUrl} handleOpenFile={handleOpenFile} />
-    <div className="flex flex-col justify-between gap-7 overflow-y-auto">
-      <div className="relative mt-4 flex flex-col justify-between">
-        <Button
-          variant="text"
-          color="indigo"
-          onClick={() => fetchNextPage()}
-          className="!absolute -top-7 left-[20%] z-50 max-w-fit dark:text-slate-100 md:left-[35%]"
-          disabled={isFetchingNextPage}
-        >
-          {isFetchingNextPage ? (
-            <Spinner color="green" className="mx-auto" />
-          ) : (
-            <Translate>Load more older messages</Translate>
-          )}
-        </Button>
-        <div className="hide-scroll-bar relative h-[78vh] overflow-y-auto rounded-lg bg-slate-200/50 py-6 px-1 dark:bg-slate-900/50 md:h-[70vh]">
-          <ScrollableFeed>
-            {messages.length > 0 &&
-              messages
-                .slice()
-                .reverse()
-                .map((message, index) =>
-                  message.fromSelf ? (
-                    <SendMessage
-                      key={index}
-                      index={index}
-                      message={message}
-                    />
-                  ) : (
-                    <ComingMessage
-                      key={index}
-                      index={index}
-                      message={message}
-                    />
-                  ),
-                )}
-
-          </ScrollableFeed>
         </div>
       </div>
-    </div>
-    <ChatInput socket={socket} handleSendMsg={sendMessage} currentChat={currentChat} isRelyingToMessage={isRelyingToMessage} setIsRelyingToMessage={setIsRelyingToMessage} />
-  </section>
-);
+      <ChatInput socket={socket} handleSendMsg={sendMessage} currentChat={currentChat} isRelyingToMessage={isRelyingToMessage} setIsRelyingToMessage={setIsRelyingToMessage} />
+    </section>
+  );
 };
 
 export default Messages;
